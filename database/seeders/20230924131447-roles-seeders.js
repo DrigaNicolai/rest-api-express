5a36de79291151444453bf5cb9173c41463bbbab
DrigaNicolai <just.whyme7@gmail.com>
2023-09-24 16:52:52 +0300

    Add seeders for role collection

---
+++ database/seeders/20230924131447-roles-seeders.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const roles = require("../../data/seeders/roles-seeders.json");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("roles", roles, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('roles', null, {});
+  }
+};
