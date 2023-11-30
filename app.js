b535038d1f00adec323aa8cbc32a3f0e09284a82
DrigaNicolai <just.whyme7@gmail.com>
2023-11-30 20:37:37 +0200

    Implement global auth middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = require("./api/routes/index");
 const validationMiddleware = require("./api/middleware/validationMiddleware.js");
+const authMiddleware = require("./api/middleware/authMiddleware.js");
 const swaggerDocs = require("./utils/swagger.js");
 
 require('dotenv').config();
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/", router);
 app.use(validationMiddleware);
+app.use(authMiddleware);
 
 swaggerDocs(app);
 
