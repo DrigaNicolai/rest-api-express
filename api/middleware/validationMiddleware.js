ad72464daae15d9b4477de85956ce6ecdd9cba8d
DrigaNicolai <just.whyme7@gmail.com>
2023-11-30 20:37:18 +0200

    Update validation middleware

--- api/middleware/validationMiddleware.js
+++ api/middleware/validationMiddleware.js
@@ -4,7 +4,7 @@ const validationMiddleware = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({message: "Error while sending request", errors});
+    return res.status(422).json({message: "Error while sending request", errors});
   }
 
   next();
