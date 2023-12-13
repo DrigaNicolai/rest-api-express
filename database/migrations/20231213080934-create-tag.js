02a7d3276ee46e6d46f7f0830a8a0edc5f875dd6
DrigaNicolai <just.whyme7@gmail.com>
2023-12-13 10:27:10 +0200

    Create tag migration file

---
+++ database/migrations/20231213080934-create-tag.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tags');
+  }
+};
